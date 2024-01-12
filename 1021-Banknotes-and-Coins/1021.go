c1770bda5d17eb24a65e5a2db52cdcb63fd1c225
Flavio <flavio.a.s@live.com>
2024-01-12 17:23:25 -0300

    solved problem 1021 - git add .! Was Hardgit add .!

--- 1021-Banknotes-and-Coins/1021.go
+++ 1021-Banknotes-and-Coins/1021.go
@@ -3,19 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	var banknotes = [6]float64{100.00, 50, 20, 10, 5, 2}
+	var banknotes = [6]int{100, 50, 20, 10, 5, 2}
+	var bankcoins = [6]float64{100.00, 50.00, 25.00, 10.00, 5.00, 1.00}
+
 	var money float64
 	var notas int
-	money = 548.97
 	fmt.Scan(&money)
 
-	fmt.Print("NOTAS\n")
+	fmt.Print("NOTAS:\n")
 	for i := 0; i < len(banknotes); i++ {
 
 		notas = int(money) / banknotes[i]
-		fmt.Printf("%d nota(s) de %d.00\n", notas, banknotes[i])
+		fmt.Printf("%d nota(s) de R$ %d.00\n", notas, (banknotes[i]))
 		if money >= float64(banknotes[i]) {
-			money = float64(banknotes[i]*notas) - money
+			money = money - float64(banknotes[i]*notas)
 		}
 	}
+	money *= 100.01
+	fmt.Print("MOEDAS:\n")
+
+	for i := 0; i < len(banknotes); i++ {
+
+		notas = int(money) / int(bankcoins[i])
+		fmt.Printf("%d moeda(s) de R$ %.2f\n", notas, (bankcoins[i] / 100))
+		if money >= float64(bankcoins[i]) {
+			money = money - (bankcoins[i] * float64(notas))
+		}
+	}
+
 }
