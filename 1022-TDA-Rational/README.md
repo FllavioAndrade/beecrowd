c2b0fa9ffc9a089137051f7490a7fb84f551693b
Flavio <flavio.a.s@live.com>
2024-02-01 18:56:17 -0300

    solved problem 1024

--- 1022-TDA-Rational/README.md
+++ 1022-TDA-Rational/README.md
@@ -4,10 +4,10 @@ You were invited to do a little job for your Mathematic teacher. The job is to r
 
 Considering N1 and D1 as numerator and denominator of the first fraction, follow the orientation about how to do each one of these 4 operations:
 
-Sum: (N1*D2 + N2*D1) / (D1*D2)
-Subtraction: (N1*D2 - N2*D1) / (D1*D2)
-Multiplication: (N1*N2) / (D1*D2)
-Division: (N1/D1) / (N2/D2), that means (N1*D2)/(N2*D1)
+Sum: (N1 * D2 + N2 * D1) / (D1 * D2)
+Subtraction: (N1 * D2 - N2 * D1) / (D1 * D2)
+Multiplication: (N1 * N2) / (D1 * D2)
+Division: (N1/D1) / (N2/D2), that means (N1 * D2)/(N2 * D1)
 
 ### Input
 The input contains several cases of test. The first value is an integer N (1 ≤ N ≤ 1*104), indicating the amount of cases of test that must be read. Each case of test contains a rational value X (1 ≤ X ≤ 1000), an operation (-, +, * or /) and another rational value Y (1 ≤ Y ≤ 1000).
--- 1024-Encryption/1024.py
+++ 1024-Encryption/1024.py
@@ -1,12 +1,35 @@
+import math
+
 n = int(input())
 
-for i in range(n):
-    message = input()
-    
-    message[0] = (message[0])
-    encrypt = ""
-    print(message)
-    for j in range(len(message),0, -1):
-        encrypt += message[j]
+for i in range (n):
+    text = input()
+    text = list(text)
+    encryption = []
+    h = len(text)
+    qtd = math.floor(h/2)
 
-    print(encrypt) 
+    for j in range(h):
+        if  (ord(text[j]) >= 65 and ord(text[j]) <= 176):
+            text[j] = chr(ord(text[j]) + 3)
+
+    if qtd%2 == 0:
+        qtd = qtd-1
+        for j in range(h-1,-1,-1):
+            encryption.append(text[j])
+            if j <= qtd+1:
+                encryption[qtd] = (chr(ord(encryption[qtd]) -1))
+                qtd = qtd + 1
+        
+    else:
+        for j in range(h-1,-1,-1):
+            encryption.append(text[j])
+            print(qtd,j)
+            if j <= qtd:
+                encryption[qtd] = (chr(ord(encryption[qtd]) -1))
+                
+                qtd = qtd + 1
+    
+    print(len(encryption))
+    text = ''.join(map(str, encryption))
+    print(text,end='\n')
